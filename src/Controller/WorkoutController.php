85b0bdc45c89de0df5f4beda665dfe96323c999d
Augustinas Čiuta <augustinas.ciuta@tesonet.com>
2021-01-24 19:26:47 +0200

    Add access check

--- src/Controller/WorkoutController.php
+++ src/Controller/WorkoutController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Workout;
+use App\Form\WorkoutFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +15,21 @@ class WorkoutController extends AbstractController
     /**
      * @Route("/workout", name="workout")
      */
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $form = $this->createForm(WorkoutFormType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $workout = $data->setPerformedAt(new \DateTime());
+
+            $em->persist($workout);
+            $em->flush();
+        }
         return $this->render('workout/index.html.twig', [
-            'controller_name' => 'WorkoutController',
+            'workoutForm' => $form->createView(),
         ]);
     }
 }
